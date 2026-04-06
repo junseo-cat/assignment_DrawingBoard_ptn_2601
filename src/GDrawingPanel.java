df43e7f3d2bbe9eefdcc822cf8a150092327eb07
junseokim <dev.barcat@gmail.com>
2026-04-07 00:38:20 +0900

    Modify : 교수님공유버전으로 수정 / 과제 적용

--- src/GDrawingPanel.java
+++ src/GDrawingPanel.java
@@ -1,60 +1,114 @@
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 
 public class GDrawingPanel extends JPanel {
-    //현재 도구
-    private String currentShape;
-
+    // constructors
     public GDrawingPanel() {
-        super();
-        this.setBackground(Color.CYAN);
-        this.currentShape = null;   //처음에는 아무 도구 선택 안됨
+        this.setBackground(Color.WHITE);
 
-        //네모 그리기
-        // 마우스의 동작을 감지 리스너 추가
-        this.addMouseListener(new java.awt.event.MouseAdapter() {
-
-            // 시작점을 기억 변수
-            int startX, startY;
-
-            // 마우스를 '눌렀을 때' 실행되는 부분
-            @Override
-            public void mousePressed(java.awt.event.MouseEvent e) {
-                // 현재 위치를
-                startX = e.getX();
-                startY = e.getY();
-            }
-
-            // 마우스를 '놓았을 때' 실행되는 부분
-            @Override
-            public void mouseReleased(java.awt.event.MouseEvent e) {
-                // 끝점 좌표 가져오기
-                int endX = e.getX();
-                int endY = e.getY();
-
-                // 마우스를 오른쪽 아래가 아닌, 왼쪽 위로 역방향 드래그할 수도 있으므로
-                // Math.min을 써서 항상 가장 작은 X, Y(즉, 왼쪽 위 모서리)를 찾기
-                int x = Math.min(startX, endX);
-                int y = Math.min(startY, endY);
-
-                // Math.abs(절댓값) / 실제 길이를 구하기
-                int width = Math.abs(startX - endX);
-                int height = Math.abs(startY - endY);
-
-                // 패널의 도화지(Graphics)
-                java.awt.Graphics g = getGraphics();
-
-                if (currentShape == null) {
-                    return;
-                } else if (currentShape.equals("rectangle")) {
-                    g.drawRect(x, y, width, height);
-                }
-            }
-        });
+        MouseHandler mouseHandler = new MouseHandler();
+        this.addMouseListener(mouseHandler);
+        this.addMouseMotionListener(mouseHandler);
     }
 
-    //외부에서 도구 변경하기
-    public void setCurrentShape(String currentShape) {
-        this.currentShape = currentShape;
+    private int x0, y0;
+    private int x1, y1;
+    private BufferedImage bufferImage;
+    private void startRectangularShape(int x, int y) {
+        this.x0 = x;
+        this.y0 = y;
+
+        if (this.getWidth() <= 0 || this.getHeight() <= 0) {
+            return;
+        }
+
+        if (this.bufferImage == null
+                || this.bufferImage.getWidth() != this.getWidth()
+                || this.bufferImage.getHeight() != this.getHeight()) {
+            this.bufferImage = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D bufferGraphics = this.bufferImage.createGraphics();
+            bufferGraphics.setColor(this.getBackground());
+            bufferGraphics.fillRect(0, 0, this.getWidth(), this.getHeight());
+            bufferGraphics.dispose();
+        }
+
+    }
+    private void finishRectangularShape(int x, int y) {
+        this.x1 = x;
+        this.y1 = y;
+
+
+        Graphics2D bufferGraphics = this.bufferImage.createGraphics();
+        bufferGraphics.setColor(this.getBackground());
+        bufferGraphics.fillRect(0, 0, this.getWidth(), this.getHeight());
+        bufferGraphics.setColor(Color.BLACK);
+        bufferGraphics.drawRect(this.x0, this.y0, this.x1 - this.x0, this.y1 - this.y0);
+        bufferGraphics.dispose();
+
+        Graphics2D panelGraphics = (Graphics2D) this.getGraphics();
+        if (panelGraphics != null) {
+            panelGraphics.drawImage(this.bufferImage, 0, 0, null);
+            panelGraphics.dispose();
+        }
+
+    }
+
+    // 한번 클릭후 그리기로 들어왔을때 한번 클릭을 무시시킬 변수
+    private boolean isDrawing = false;
+    private boolean isDrag = false;
+
+    private class MouseHandler implements MouseListener, MouseMotionListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+//			if ( e.getClickCount() == 1 && ! isDrawing ) {
+//				isDrawing = true;
+//				startRectangularShape(e.getX(), e.getY());
+//			} else
+//
+            if ( e.getClickCount() == 2 && isDrawing ) {
+                isDrawing = false;
+                finishRectangularShape(e.getX(), e.getY());
+            }
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+            if ( !isDrawing ) {
+                isDrawing = true;
+                startRectangularShape(e.getX(), e.getY());
+            }
+
+        }
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            isDrag = true;
+            isDrawing =false;
+            finishRectangularShape(e.getX(), e.getY());
+        }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if ( isDrag ) {
+                isDrag = false;
+                finishRectangularShape(e.getX(), e.getY());
+            }
+
+        }
+        @Override
+        public void mouseMoved(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+
     }
 }
